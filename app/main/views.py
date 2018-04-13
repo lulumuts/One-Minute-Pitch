5555a8952099cd37c54e241291bb2d8d62301aae
Lulu <lulumutuli24@gmail.com>
2018-04-13 15:31:08 +0300

    views.py updated with main.route to render template to index.html

---
+++ app/main/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from . import main
+
+
+@main.route('/'):
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to One Minute Pitch'
+
+
+    return  render_template('index.html', title = title)
