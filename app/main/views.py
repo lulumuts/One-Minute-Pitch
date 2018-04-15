e18fac092db2f32892469803e05ee59df49214fe
Lulu <lulumutuli24@gmail.com>
2018-04-15 10:10:25 +0300

    index.html formatting updated with category pitches

--- app/main/views.py
+++ app/main/views.py
@@ -8,7 +8,7 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    title = 'Home - Welcome to One Minute Pitch'
+    title = 'Welcome to One Minute Pitch'
 
 
     return  render_template('index.html', title=title)
